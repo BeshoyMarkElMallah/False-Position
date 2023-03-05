7152d3f39b41dd7872a12bdd9d2ca35a6215bb43
BeshoyMarkElMallah <bisho.king.980@gmail.com>
2023-03-05 17:20:40 +0200

    False Position method

---
+++ False_Position.py
@@ -0,0 +1,42 @@
+import math as m
+
+error = 100
+xr_old = 0
+
+xl = float(input("Enter the lower limit: "))
+xu = float(input("Enter the upper limit: "))
+
+
+
+while error > 0.001:
+    
+    fxl = m.pow((m.pow(xl, 2)+1), 2)-25
+    print("The value of f(xl) is: ", fxl)
+    fxu = m.pow((m.pow(xu, 2)+1), 2)-25
+    print("The value of f(xu) is: ", fxu)
+    xr_new = ((xu*fxl)-(xl*fxu))/(fxl-fxu)
+    print("The root is: ", xr_new)
+    
+    fxr = m.pow((m.pow(xr_new, 2)+1), 2)-25
+    print("The value of f(xr) is: ", fxr)
+
+    mul_fxl_fxr = fxl*fxr
+    
+    if mul_fxl_fxr < 0:
+        xu = xr_new
+    elif mul_fxl_fxr > 0:
+        xl = xr_new
+
+    if xr_old != 0:
+        error = ((xr_new - xr_old)/xr_new) * 100
+        
+        if(error < 0):
+            error = error * -1
+        else:
+            error = error
+        
+    xr_old = xr_new
+
+
+print("The root is: ", xr_new)
+print("The error is: ", error)
